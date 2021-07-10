73de87ca9a8b2003c422c8499740313b299f81ae
Estera <estera.maftei94@gmail.com>
2021-07-10 21:21:44 +0100

    js file added and navigation border added on scroll

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+const links = document.querySelectorAll('.navbar-nav a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', ()=>{
+    let current = '';
+    sections.forEach(section =>{
+        let sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        console.log(sectionHeight)
+        if(pageYOffset >= (sectionTop - sectionHeight / 4)){
+            current = section.getAttribute('id')
+        }
+    })
+
+    links.forEach( a => {
+        a.classList.remove('active')
+        if(a.classList.contains(current)){
+            a.classList.add('active')
+        }
+    })
+})
