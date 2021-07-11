303ca0a70807d5edafaf05ccada802c3a5c84a0b
Estera <estera.maftei94@gmail.com>
2021-07-11 14:59:51 +0100

    added navigation on small screen and waves svg

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ window.addEventListener('scroll', ()=>{
     sections.forEach(section =>{
         let sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        console.log(sectionHeight)
         if(pageYOffset >= (sectionTop - sectionHeight / 4)){
             current = section.getAttribute('id')
         }
@@ -18,4 +17,47 @@ window.addEventListener('scroll', ()=>{
             a.classList.add('active')
         }
     })
+})
+
+
+const controller = new ScrollMagic.Controller();
+
+ new ScrollMagic.Scene({
+    triggerElement:'.title',
+    triggerHook: 0.4,
+    // reverse: false
+})// .addIndicators({ colorStart: 'white'})
+    .setClassToggle('.esteraName', 'colorTransition')
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement:'.aboutTitle',
+    triggerHook: 0.6
+})  .setClassToggle('.aboutTitle', 'colorTransition')
+    .addTo(controller)
+
+new ScrollMagic.Scene({
+    triggerElement:'.portfolioTitle',
+    triggerHook: 0.6
+})  .setClassToggle('.portfolioTitle', 'colorTransition')
+    .addTo(controller)
+
+new ScrollMagic.Scene({
+    triggerElement:'.contactTitle',
+    triggerHook: 0.6
+})  .setClassToggle('.contactTitle', 'colorTransition')
+    .addTo(controller)
+
+
+const hamburger = document.querySelector('.hamburger-button')
+const navLinks = document.querySelector(".navbar-nav");
+const aNavigation = document.querySelectorAll(".navbar-nav a");
+
+hamburger.addEventListener('click', ()=> {
+        navLinks.classList.toggle("open");
+        aNavigation.forEach(a => {
+            a.addEventListener('click', ()=>{
+               navLinks.classList.remove("open");
+            })
+        });
 })
